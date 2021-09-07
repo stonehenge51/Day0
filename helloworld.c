b1b04dc585ec123beafbb01bc54b9f74c515b872
stonehenge51 <benjamin.mcclennon@email.kpu.ca>
2021-09-07 09:59:26 -0700

    Created Project day0

---
+++ helloworld.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+
+int main()
+{
+	int x; //declaration
+	x = 0; //Intialization
+	int y = 4; //above combined
+
+	int z;
+	z = x + y;
+
+	printf("Hello World!\n %d", z);
+	
+
+	return 0;
+}
