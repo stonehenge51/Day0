e60325b59d075649aaf0fd6e7a6d2936554686f0
stonehenge51 <benjamin.mcclennon@email.kpu.ca>
2021-09-07 11:41:23 -0700

    improvements to mean and stddev calc

--- meanandstddev.c
+++ meanandstddev.c
@@ -6,36 +6,27 @@ int main()
 {
 	const int n = 10;
 	double x[] = {1,2,3,4,5,6,7,8,9,0};
-	double y = 0;
-	double z = 0;
+	double total = 0;
+	double differences = 0;
 	double mean = 0;
 	double stddev = 0;
+	int i = 0;
 
-	for(int i = 0;i <= n; i++)
+	for(i = 0;i < n; i++)
 	{
-		if(i == n)
-		{
-			mean = y / i;
-		}
-		else
-		{
-			y = y + x[i];
-		}
+		total = total + x[i];
 	}
 
-	for(int i = 0;i <= n; i++)
+	mean = total / n;
+
+	for(i = 0;i < n; i++)
 	{
-		if(i == n)
-		{
-			z = z /(n - 1);
-			stddev = sqrt(z);
-		}
-		else
-		{
-			z = z + pow(x[i]-mean,2);
-		}
+		differences = differences + pow(mean - x[i],2);
 	}
+
+	stddev = sqrt(differences /(n - 1));
 
 	printf("The mean is %0.4f\nThe standard of deviation is %0.4f", mean, stddev);
 				
+	
 }
